e3e6ab7426028980a299748ddc75b80cf1ad65e0
rheasan <shreyas.chavan@practo.com>
2024-06-26 10:30:32 +0530

    added sample sentry app

---
+++ sentry_test/README.md
@@ -0,0 +1,5 @@
+### Q1. Create an application and integrate sentry with it. Push error messages to the sentry
+```bash
+cargo run
+```
+![Q1](./screenshots/sentry_error.png)
---
+++ sentry_test/src/main.rs
@@ -0,0 +1,15 @@
+use std::{thread::sleep, time::Duration};
+
+use dotenv;
+fn main() {
+    dotenv::dotenv().ok();
+    let _guard = sentry::init((std::env::var("SENTRY_DSN").unwrap(), sentry::ClientOptions {
+      release: sentry::release_name!(),
+      ..Default::default()
+    }));
+  
+    // Sentry will capture this
+    println!("Panicking in 3sec");
+    sleep(Duration::from_secs(3));
+    panic!("Everything is on fire!");
+  }
