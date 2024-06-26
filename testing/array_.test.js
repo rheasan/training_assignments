5dbb496e3ef2236cee5b88e4b0df5808650ed883
rheasan <shreyas.chavan@practo.com>
2024-06-26 15:50:02 +0530

    add more test cases

--- testing/array_.test.js
+++ testing/array_.test.js
@@ -15,6 +15,12 @@ class Array_ {
     push = (item) => {
         this.array.push(item);
     }
+    pop = () => {
+        if(this.array.length == 0) {
+            throw new Error("Cannot pop from empty Array_");
+        }
+        return this.array.pop();
+    }
 }
 
 test("should throw when accessing out of bounds", () => {
@@ -31,3 +37,27 @@ test("array index access works", () => {
     arr.push(1);
     expect(arr.at(0)).toBe(1);
 });
+
+test("should throw when accessing undefined or null index", () => {
+    expect(() => {
+        let arr = new Array_();
+        arr.at(null);
+    }).toThrow("Index not specified");
+    expect(() => {
+        let arr = new Array_();
+        arr.at();
+    }).toThrow("Index not specified");
+});
+
+test("pop works", () => {
+    let arr = new Array_();
+    arr.push(1);
+    expect(arr.pop()).toBe(1);
+});
+
+test("pop throws when popping from empty array", () => {
+    expect(() => {
+        let arr = new Array_();
+        arr.pop();
+    }).toThrow("Cannot pop from empty Array_");
+})
